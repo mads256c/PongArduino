0ef7295d53390300b8664e1255cf958b097934d8
Mads Mogensen <mads256c@elev.aatg.dk>
2018-08-25 16:39:22 +0200

    Score

--- PongArduino/PongArduino.ino
+++ PongArduino/PongArduino.ino
@@ -14,6 +14,118 @@
 
 #define PADDLE_LEN 3
 
+const PROGMEM bool Dash[] = { 0, 0, 0, 0, 0, 0, 0, 0,
+							  0, 0, 0, 0, 0, 0, 0, 0,
+							  0, 0, 0, 0, 0, 0, 0, 0,
+							  0, 1, 1, 1, 1, 1, 1, 0,
+							  0, 1, 1, 1, 1, 1, 1, 0,
+							  0, 0, 0, 0, 0, 0, 0, 0,
+							  0, 0, 0, 0, 0, 0, 0, 0,
+							  0, 0, 0, 0, 0, 0, 0, 0 };
+
+const PROGMEM bool Zero[] = { 0, 0, 0, 0, 0, 0, 0, 0,
+							  0, 0, 0, 1, 1, 0, 0, 0,
+							  0, 0, 1, 0, 0, 1, 0, 0,
+							  0, 0, 1, 0, 0, 1, 0, 0,
+							  0, 0, 1, 0, 0, 1, 0, 0,
+							  0, 0, 1, 0, 0, 1, 0, 0,
+							  0, 0, 0, 1, 1, 0, 0, 0,
+							  0, 0, 0, 0, 0, 0, 0, 0 };
+
+const PROGMEM bool One[] = {  0, 0, 0, 0, 0, 0, 0, 0,
+							  0, 0, 0, 0, 1, 0, 0, 0,
+							  0, 0, 0, 1, 1, 0, 0, 0,
+							  0, 0, 0, 0, 1, 0, 0, 0,
+							  0, 0, 0, 0, 1, 0, 0, 0,
+							  0, 0, 0, 0, 1, 0, 0, 0,
+							  0, 0, 0, 1, 1, 1, 0, 0,
+							  0, 0, 0, 0, 0, 0, 0, 0 };
+
+const PROGMEM bool Two[] = {  0, 0, 0, 0, 0, 0, 0, 0,
+							  0, 0, 0, 1, 1, 0, 0, 0,
+							  0, 0, 1, 0, 0, 1, 0, 0,
+							  0, 0, 0, 0, 0, 1, 0, 0,
+							  0, 0, 0, 0, 1, 0, 0, 0,
+							  0, 0, 0, 1, 0, 0, 0, 0,
+							  0, 0, 1, 1, 1, 1, 0, 0,
+							  0, 0, 0, 0, 0, 0, 0, 0 };
+
+const PROGMEM bool Three[] = {0, 0, 0, 0, 0, 0, 0, 0,
+							  0, 0, 1, 1, 1, 0, 0, 0,
+							  0, 0, 0, 0, 0, 1, 0, 0,
+							  0, 0, 0, 1, 1, 0, 0, 0,
+							  0, 0, 0, 0, 0, 1, 0, 0,
+							  0, 0, 0, 0, 0, 1, 0, 0,
+							  0, 0, 1, 1, 1, 0, 0, 0,
+							  0, 0, 0, 0, 0, 0, 0, 0 };
+
+const PROGMEM bool Four[] = { 0, 0, 0, 0, 0, 0, 0, 0,
+							  0, 0, 1, 0, 0, 1, 0, 0,
+							  0, 0, 1, 0, 0, 1, 0, 0,
+							  0, 0, 1, 1, 1, 1, 0, 0,
+							  0, 0, 0, 0, 0, 1, 0, 0,
+							  0, 0, 0, 0, 0, 1, 0, 0,
+							  0, 0, 0, 0, 0, 1, 0, 0,
+							  0, 0, 0, 0, 0, 0, 0, 0 };
+
+const PROGMEM bool Five[] = { 0, 0, 0, 0, 0, 0, 0, 0,
+							  0, 0, 1, 1, 1, 1, 0, 0,
+							  0, 0, 1, 0, 0, 0, 0, 0,
+							  0, 0, 0, 1, 1, 0, 0, 0,
+							  0, 0, 0, 0, 0, 1, 0, 0,
+							  0, 0, 0, 0, 0, 1, 0, 0,
+							  0, 0, 1, 1, 1, 0, 0, 0,
+							  0, 0, 0, 0, 0, 0, 0, 0 };
+
+const PROGMEM bool Six[] = {  0, 0, 0, 0, 0, 0, 0, 0,
+							  0, 0, 0, 1, 1, 1, 0, 0,
+							  0, 0, 1, 0, 0, 0, 0, 0,
+							  0, 0, 1, 1, 1, 0, 0, 0,
+							  0, 0, 1, 0, 0, 1, 0, 0,
+							  0, 0, 1, 0, 0, 1, 0, 0,
+							  0, 0, 0, 1, 1, 0, 0, 0,
+							  0, 0, 0, 0, 0, 0, 0, 0 };
+
+const PROGMEM bool Seven[] = {0, 0, 0, 0, 0, 0, 0, 0,
+							  0, 0, 1, 1, 1, 1, 0, 0,
+							  0, 0, 0, 0, 0, 1, 0, 0,
+							  0, 0, 0, 0, 1, 0, 0, 0,
+							  0, 0, 0, 1, 0, 0, 0, 0,
+							  0, 0, 0, 1, 0, 0, 0, 0,
+							  0, 0, 0, 1, 0, 0, 0, 0,
+							  0, 0, 0, 0, 0, 0, 0, 0 };
+
+const PROGMEM bool Eight[] = {0, 0, 0, 0, 0, 0, 0, 0,
+							  0, 0, 0, 1, 1, 0, 0, 0,
+							  0, 0, 1, 0, 0, 1, 0, 0,
+							  0, 0, 0, 1, 1, 0, 0, 0,
+							  0, 0, 1, 0, 0, 1, 0, 0,
+							  0, 0, 1, 0, 0, 1, 0, 0,
+							  0, 0, 0, 1, 1, 0, 0, 0,
+							  0, 0, 0, 0, 0, 0, 0, 0 };
+
+const PROGMEM bool Nine[] = { 0, 0, 0, 0, 0, 0, 0, 0,
+							  0, 0, 0, 1, 1, 0, 0, 0,
+							  0, 0, 1, 0, 0, 1, 0, 0,
+							  0, 0, 1, 0, 0, 1, 0, 0, 
+							  0, 0, 0, 1, 1, 0, 0, 0,
+							  0, 0, 0, 0, 0, 1, 0, 0,
+							  0, 0, 1, 1, 1, 0, 0, 0,
+							  0, 0, 0, 0, 0, 0, 0, 0 };
+
+const PROGMEM bool Cross[] = {0, 0, 0, 0, 0, 0, 0, 0,
+							  0, 1, 0, 0, 0, 0, 1, 0,
+							  0, 0, 1, 0, 0, 1, 0, 0,
+							  0, 0, 0, 1, 1, 0, 0, 0,
+							  0, 0, 0, 1, 1, 0, 0, 0,
+							  0, 0, 1, 0, 0, 1, 0, 0,
+							  0, 1, 0, 0, 0, 0, 1, 0,
+							  0, 0, 0, 0, 0, 0, 0, 0 };
+
+
+uint8_t score1 = 0;
+uint8_t score2 = 0;
+
 uint8_t paddle1Pos = 0;
 uint8_t paddle2Pos = 0;
 
@@ -21,14 +133,6 @@ bool ballUp = false, ballRight = false;
 uint8_t ballXPos = 4, ballYPos = 4;
 
 int pattern = 0;
-
-void resetBall()
-{
-	ballXPos = random(3, 5);
-	ballYPos = random(3, 5);
-	ballUp = ballYPos == 3;
-	ballRight = ballXPos == 3;
-}
 
 void applyShift()
 {
@@ -41,17 +145,116 @@ void resetScreen()
 {
 	pattern = 0;
 	applyShift();
-	for (int i = 0; i < 8; ++i)
+	for (uint8_t i = 0; i < 8; ++i)
 	{
 		digitalWrite(i + 2, HIGH);
 	}
 }
 
+void drawArray(const bool* array)
+{
+	
+	for (uint8_t x = 0; x < 8; ++x)
+	{
+		resetScreen();
+		for (uint8_t y = 0; y < 8; ++y)
+		{
+			bool b = pgm_read_byte_near(array + 8 * y + x );
+
+			//bool b = array[8 * y + x];
+			//Serial.print(b);
+			if (b)
+				digitalWrite(7 - y + 2, LOW);
+		}
+		//Serial.println();
+		pattern = (1 << x);
+		applyShift();
+		delay(1);
+	}
+}
+
+const bool* numberToArray(const uint8_t number)
+{
+	switch (number)
+	{
+	case 0: return Zero;
+	case 1: return One;
+	case 2: return Two;
+	case 3: return Three;
+	case 4: return Four;
+	case 5: return Five;
+	case 6: return Six;
+	case 7: return Seven;
+	case 8: return Eight;
+	case 9: return Nine;
+
+	default: return Cross;
+	}
+
+}
+
+void countDown()
+{
+	for (uint8_t i = 3; i > 0; --i)
+	{
+		const unsigned long before = millis();
+		while (millis() - before < 1000)
+		{
+			drawArray(numberToArray(i));
+		}
+	}
+}
+
+void displayScore()
+{
+
+	if (score1 > 9 || score2 > 9)
+	{
+		score1 = 0;
+		score2 = 0;
+
+		countDown();
+	}
+
+	unsigned long before = millis();
+
+	while(millis() - before < 1000)
+	{
+		drawArray(numberToArray(score1));
+	}
+
+	before = millis();
+
+	while (millis() - before < 1000)
+	{
+		drawArray(Dash);
+	}
+
+	before = millis();
+
+	while (millis() - before < 1000)
+	{
+		drawArray(numberToArray(score2));
+	}
+
+	
+}
+
+void resetBall()
+{
+	ballXPos = random(3, 5);
+	ballYPos = random(3, 5);
+	ballUp = ballYPos == 3;
+	ballRight = ballXPos == 3;
+}
+
+
+
 void drawPaddle1()
 {
 	pattern |= (1 << 0);
 
-	for (int i = 0; i < PADDLE_LEN; i++)
+	for (uint8_t i = 0; i < PADDLE_LEN; i++)
 	{
 		digitalWrite(i + paddle1Pos + 2, LOW);
 	}
@@ -61,7 +264,7 @@ void drawPaddle2()
 {
 	pattern |= (1 << 7);
 
-	for (int i = 0; i < PADDLE_LEN; i++)
+	for (uint8_t i = 0; i < PADDLE_LEN; i++)
 	{
 		digitalWrite(i + paddle2Pos + 2, LOW);
 	}
@@ -77,12 +280,12 @@ void flashScreen()
 {
 	resetBall();
 
-	for (int i = 0; i < 4; ++i)
+	for (uint8_t i = 0; i < 4; ++i)
 	{
 		pattern = 255;
 		applyShift();
 
-		for (int j = 0; j < 8; ++j)
+		for (uint8_t j = 0; j < 8; ++j)
 		{
 			digitalWrite(j + 2, LOW);
 		}
@@ -92,7 +295,7 @@ void flashScreen()
 		pattern = 0;
 		applyShift();
 
-		for (int j = 0; j < 8; ++j)
+		for (uint8_t j = 0; j < 8; ++j)
 		{
 			digitalWrite(j + 2, HIGH);
 		}
@@ -113,9 +316,9 @@ void updateBall()
 	else if (ballXPos == 0)
 		ballRight = true;
 	if (!ballRight && ballXPos <= 1 && paddle1Pos <= ballYPos && paddle1Pos + PADDLE_LEN >= ballYPos) ballRight = true;
-	else if (ballXPos == 0) flashScreen();
+	else if (ballXPos == 0) { score1++; displayScore(); resetBall(); }
 	if (ballRight && ballXPos >= 6 && paddle2Pos <= ballYPos && paddle2Pos + PADDLE_LEN >= ballYPos) ballRight = false;
-	else if (ballXPos == 7) flashScreen();
+	else if (ballXPos == 7) { score2++; displayScore(); resetBall(); }
 
 	//Serial.println(paddle1Pos);
 	//Serial.println(ballYPos);
@@ -160,7 +363,7 @@ void setup() {
 	shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, 0);
 	digitalWrite(LATCH_PIN, HIGH);
 
-	for (int i = 0; i < 8; ++i)
+	for (uint8_t i = 0; i < 8; ++i)
 	{
 		pinMode(i + 2, OUTPUT);
 		digitalWrite(i + 2, HIGH);
@@ -169,6 +372,7 @@ void setup() {
 	paddleTimer = ballTimer;
 
 	resetBall();
+	countDown();
 }
 
 
